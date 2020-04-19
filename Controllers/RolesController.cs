fe9b509c314605d117d5370a7adb4735ce1a0632
artandfi <artyom.fisunenko@gmail.com>
2020-04-19 16:49:03 +0300

    Added adding new roles feature

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -9,13 +9,46 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Lexiconn.Controllers
 {
+
     public class RolesController : Controller
     {
+        private const string ERR_DUPL_ROLE = "Така роль вже існує";
         private readonly RoleManager<IdentityRole> _roleManager;
+        
         public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
         {
             _roleManager = roleManager;
         }
         public IActionResult Index() => View(_roleManager.Roles.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!_roleManager.Roles.Any(r => r.Name.Equals(model.Name)))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError("Name", ERR_DUPL_ROLE);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ ViewModels/RoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexiconn.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage = "Поле необхідно заповнити")]
+        [StringLength(25)]
+        [Display(Name = "Назва")]
+        public string Name { get; set; }
+    }
+}
